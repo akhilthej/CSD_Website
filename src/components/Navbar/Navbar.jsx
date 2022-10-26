f89de25bb898bae488a6e68fc39927825affba3f
akhilthej <67720804+akhilthej@users.noreply.github.com>
2022-10-27 00:51:42 +0530

    FinalFix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import {Link} from 'react-router-dom';
 import { Menu, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/solid';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -26,10 +25,11 @@ const Navbar = () => {
             <div>
               <Menu.Button className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'>
                 More
-                <ChevronDownIcon
-                  className='-mr-1 ml-2 h-5 w-5'
-                  aria-hidden='true'
-                />
+                <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 mt-0.5 ml-1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+
+
               </Menu.Button>
             </div>
 
