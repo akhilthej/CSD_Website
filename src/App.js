1884672130206d538fca75f23612ab9cbf3556d2
leormedia <67720804+akhilthej@users.noreply.github.com>
2022-10-26 11:48:59 +0530

    12

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-
+import ReactGA from 'react-ga';
 import AnimatedRoutes from './components/AnimatedRoutes';
 
 
 
 
-
-
+/*---Google Analytics Tracker---*/
+const TRACKING_ID = "UA-247075028-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   
