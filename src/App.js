45e01061be77baea3983bad1c9a7530659d58e02
akhilthej <67720804+akhilthej@users.noreply.github.com>
2022-11-28 16:50:32 +0530

    ga remove

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import ReactGA from 'react-ga';
 
 import PrivacyPolicy from './components/PAGES/Aboutus/PrivacyPolicy';
 import TermsnConditions from './components/PAGES/Aboutus/TermsnConditions';
@@ -30,51 +29,47 @@ import Reactblog from './components/PAGES/Technologies/Blogpages/Reactblog';
 
 import { AnimatePresence } from 'framer-motion';
 
-
-
-/*---Google Analytics Tracker
-const TRACKING_ID = "UA-247075028-1";
-ReactGA.initialize(TRACKING_ID);---*/
-const App = () => {
+export function App() {
   const location= useLocation();
   window.scrollTo(0, 0); /*---ResetPagelocation---*/
 
-  
   return (
-    <>
-      <Navbar />
-      
-      <AnimatePresence>
-    <Routes location ={location} key={location.pathname}>
-        <Route path='/' element={<Home />} />
-        <Route path='/*' element={<Error404 />} />
-
-
-        <Route path='/services' element={<Services />} />
-
-        <Route path='/technologies' element={<Technologies />} />
-              <Route path='/reactblog' element={<Reactblog />} />
+  <>
+  <Navbar />
+    <AnimatePresence>
+      <Routes location ={location} key={location.pathname}>
         
-        <Route path='/clients' element={<Clients />} />
-        <Route path='/reviews' element={<Reviews />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/*' element={<Error404 />} />
 
-        <Route path='/getquotation' element={<GetQuotation />} />
 
-        <Route path='/aboutus' element={<Aboutus />} />
-        <Route path='/contactus' element={<Contactus />} /> 
-        <Route path='/privacypolicy' element={<PrivacyPolicy />} /> 
-        <Route path='/terms&conditions' element={<TermsnConditions />} />
-        <Route path='/disclaimer' element={<Disclaimer />} />
-        <Route path='/sitemaps' element={<Sitemaps/>} />
+          <Route path='/services' element={<Services />} />
 
-        </Routes>
-        </AnimatePresence>
-
-        <Footer />
-  
+          <Route path='/technologies' element={<Technologies />} />
+            <Route path='/reactblog' element={<Reactblog />} />
         
-    </>
+          <Route path='/clients' element={<Clients />} />
+          <Route path='/reviews' element={<Reviews />} />
+
+          <Route path='/getquotation' element={<GetQuotation />} />
+
+          <Route path='/aboutus' element={<Aboutus />} />
+          <Route path='/contactus' element={<Contactus />} /> 
+          <Route path='/privacypolicy' element={<PrivacyPolicy />} /> 
+          <Route path='/terms&conditions' element={<TermsnConditions />} />
+          <Route path='/disclaimer' element={<Disclaimer />} />
+          <Route path='/sitemaps' element={<Sitemaps/>} />
+         
+
+
+      </Routes>
+    </AnimatePresence>
+
+  <Footer />   
+</>
+    
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   
-  <HashRouter>
+  <BrowserRouter>
     <HelmetProvider>
     <App />
     </HelmetProvider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
