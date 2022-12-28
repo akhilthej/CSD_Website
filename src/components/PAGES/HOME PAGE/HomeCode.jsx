a367cb3fd13b812307362c97140f33561887e738
akhilthej <67720804+akhilthej@users.noreply.github.com>
2022-12-28 06:16:57 +0530

    New

--- src/components/PAGES/HOME PAGE/HomeCode.jsx
+++ src/components/PAGES/HOME PAGE/HomeCode.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import './Home.scss';
-import {CSDbothome,HomeHeroimage,HeroCoverBG } from '../../data/data.jsx';
+import {CSDbothome,HeroCoverBG } from '../../data/data.jsx';
 import '../../Tools/Animations.scss';
 import {codeing ,graphicsdesign ,digitalmarketing ,mobiledev ,recruitment ,brand } from '../../data/data.jsx';
 import {documentationicon ,socialicon ,analysisicon ,financeicon ,statergyicon ,businessreporticon ,}from '../../data/data.jsx';
@@ -13,81 +13,45 @@ const HomeHero = () => {
   <main>
 
 {/*herosection desktop*/}    
-    <header className='HerosectionDesktop'>
-      <div className="homehero">
-        <div className="homeimage imagehover"> <img src={HomeHeroimage} /> </div>
-          <div className="hometext ">
-            <h1>CYBER SPACE DIGITAL</h1>
-            <h2>Solution for your Business</h2>
-            <Link to="/contactus"> <button className='mx-auto block w-full rounded border bg-orange-600 py-3 px-3 text-sm font-medium text-white hover:bg-transparent hover:text-black focus:outline-none focus:ring active:text-opacity-75 sm:w-auto'>Get In-Touch</button></Link>
-          </div>
-        <div className='homeheroBG'><img src={HeroCoverBG} /></div>
-      </div>
-    </header>
-      
-{/*---hero section mobile
-      <section >
-        
-      <div className="z-0 text-center  sm:block md:hidden flex flex-col items-center justify-cente  pt-20">
-      <div className="max-w-xl mb-3   ">
-        <h1 class=" pt-5 bg-gradient-to-r  from-orange-500 via-orange-300 to-orange-500 bg-clip-text font-extrabold text-transparent text-5xl ">
-          CYBER SPACE DIGITAL
-        </h1>
-        <span class="text-black-300 text-2xl  "> Solution for your Business </span>
-      </div>
-      <div className="master-container ">
-          <div className="content center">
-            <h1 id="master">
-              <div id="master-container-scroller">
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Web Development</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">App Development</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Digital Marketing</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Product Design</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Instructional Design</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Interactive Design</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Brand Design</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">UI/ UX Design</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Social Meida</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">SEO</a>
-                </div>
-                <div className="master-container-scroller_item">
-                  <a className="cta-link" href="#">Graphic Design</a>
-                </div>
-                <div className="master-container-scroller_item" />
-              </div>
-              <div className='text-sm py-3 '>Let's Build your business today !</div>
+    <header>
+      <section class="pt-28 bg-white">
+    <div class="px-12 mx-auto max-w-7xl">
+        <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
+            <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+                <span>Cyber</span> <span class="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-orange-400 to-orange-700 lg:inline">Space</span> <span>Digital</span>
             </h1>
-          </div>
+            <p class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
+                Build your business online today !
+            </p>
+            <div class="mb-4 space-x-0 md:space-x-2 md:mb-8">
+                <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-orange-400 rounded-2xl sm:w-auto sm:mb-0">
+                    Get Started
+                    <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+                <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 rounded-2xl sm:w-auto sm:mb-0">
+                    Learn More
+                    <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg>
+                </a>
+                
+            </div>
         </div>
-        <Link to= '/contactus'>
-            <a class=" block w-full rounded border bg-orange-600 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-black focus:outline-none focus:ring active:text-opacity-75 sm:w-auto">
-                Get in-touch </a></Link>
-   
-      <img src="https://kitwind.io/assets/kometa/half-phone.png"
-        className=" pt-2 px-8 w-full mx-auto "/>
-        
-        
+        <div class="w-full mx-auto mt-20 text-center md:w-10/12 ">
+            <div class="relative z-0 w-full mt-8 ">
+                <div class="relative overflow-hidden shadow-2xl ">
+                    <div class="flex items-center flex-none px-4 bg-orange-400 rounded-b-none h-11 rounded-xl">
+                        <div class="flex space-x-1.5">
+                            <div class="w-3 h-3 border-2 border-white rounded-full"></div>
+                            <div class="w-3 h-3 border-2 border-white rounded-full"></div>
+                            <div class="w-3 h-3 border-2 border-white rounded-full"></div>
+                        </div>
+                    </div>
+                    <div img src={HeroCoverBG}/>
+                </div>
+            </div>
         </div>
-      </section>--- */}
+    </div>
+      </section>
+    </header>
 
 {/*MainFeatures */}
       <section>
