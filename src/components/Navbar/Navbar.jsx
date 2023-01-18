4d79570adff00aee6efd7ed13c2171dfc64103a5
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-01-18 12:12:32 +0530

    Navbar correction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function classNames(...classes) {
 const Navbar = () => {
   return (
     <main class ='mt-1 mx-4 sm:mx-20 rounded-2xl sticky top-0 z-50 bg-black/90  -mb-16' >
-    <div className=' w-full h-14 flex justify-between items-center pl-10 md:pl-15 text-white'>
+    <div className=' w-full h-14 flex justify-between items-center pl-10 md:pl-15 text-white '>
       <Link to='/'><img class="h-auto w-10" src={require('../../assets/Common/csd_navbarlogo.png')} alt='csd_logo' /></Link>
       
       <div class=" justify-end ">
@@ -23,7 +23,7 @@ const Navbar = () => {
         <li className='p-4'>
         <Menu as='div' className='relative inline-block text-left '>
             <div>
-              <Menu.Button className='inline-flex justify-center w-full rounded-md border border-gray-900 shadow-sm px-4 py-2 bg-black text-sm font-medium text-white hover:bg-gray-500 focus:outline-none  '>
+              <Menu.Button className='inline-flex justify-center w-full rounded-md border border-gray-900 shadow-sm px-4 py-2 bg-black text-sm font-medium text-white hover:bg-gray-700 focus:outline-none  '>
                 More
                 <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 mt-0.5 ml-1">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -40,15 +40,15 @@ const Navbar = () => {
               leaveFrom='transform opacity-100 scale-100'
               leaveTo='transform opacity-0 scale-95'
             >
-              <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-black ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-50'>
+              <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-black ring-1 ring-black ring-opacity-5 divide-y divide-gray-900 focus:outline-none z-50'>
                 <div className='py-1  md:hidden '>
                   <Menu.Item>
                     {({ active }) => (
                       <Link to='/services'><a
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
@@ -62,8 +62,8 @@ const Navbar = () => {
                       <Link to='/getquotation'><a
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
@@ -76,8 +76,8 @@ const Navbar = () => {
                       <Link to='/clients'><a
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
@@ -93,8 +93,8 @@ const Navbar = () => {
                       
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
@@ -108,8 +108,8 @@ const Navbar = () => {
                       <Link to='/reviews'><a
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
@@ -125,8 +125,8 @@ const Navbar = () => {
 
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
@@ -140,30 +140,13 @@ const Navbar = () => {
                       <Link to='/contactus'><a
                         className={classNames(
                           active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
+                            ? 'bg-gray-700 text-white'
+                            : 'text-white',
                           'block px-4 py-2 text-sm'
                         )}
                       >
                        Contact us
                       </a></Link>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className='py-1'>
-                  <Menu.Item disabled>
-                    {({ active }) => (
-                      <a
-            
-                        className={classNames(
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Downlaods
-                      </a>
                     )}
                   </Menu.Item>
                 </div>
