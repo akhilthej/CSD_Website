3794fa31b8e3c9e412980d38f47c7661b15632bd
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-01-18 11:55:01 +0530

    121

--- src/routes/Aboutus.js
+++ src/routes/Aboutus.js
@@ -8,9 +8,9 @@ const Aboutus = () => {
   return (
     <div className='Aboutus'>
       <Helmet>
-                <title>Cyber Space Digital - About Us</title>
-                <meta name="description" content='Web Development and Digital Marketing Company. We are here to build you business online. One stop for all your digital needs.'/>
-                <link rel="canonical" href="/technologies" />
+            <title>Cyber Space Digital - About Us</title>
+            <meta name="description" content='Web Development and Digital Marketing Company. We are here to build you business online. One stop for all your digital needs.'/>
+            <link rel="canonical" href="/technologies" />
       </Helmet>
     
     <About />
