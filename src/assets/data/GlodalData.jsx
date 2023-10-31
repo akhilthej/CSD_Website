166352f2940f57248da6308af466076bccd66345
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-11-01 00:58:35 +0530

    icons set

--- src/assets/data/GlodalData.jsx
+++ src/assets/data/GlodalData.jsx
@@ -20,6 +20,66 @@ import Ecommerce from '../Services_icons/WebDevelopment/ecommerce.webp'
 import WebRecamp from '../Services_icons/WebDevelopment/WebRevamp.webp'
 import CRMERM from '../Services_icons/WebDevelopment/crmermpannel.webp'
 
+import  BannerDesign from '../Services_icons/MultiMedia/Banner Design.webp';
+import  BrandIdentityDesign from '../Services_icons/MultiMedia/Brand Identity Design.webp'
+import  BrochureDesign from '../Services_icons/MultiMedia/Brochure Design.webp'
+import  BusinessCard from '../Services_icons/MultiMedia/Business Card.webp'
+import  eBookandPrintBook from '../Services_icons/MultiMedia/eBook and Print Book Cover Design.webp'
+import  IllustationServices from '../Services_icons/MultiMedia/Illustation Services.webp'
+import  InfographicDesign from '../Services_icons/MultiMedia/Infographic Design.webp'
+import  logodesign from '../Services_icons/MultiMedia/logodesign.webp'
+import  MotionGraphics from '../Services_icons/MultiMedia/Motion Graphics and Animation.webp'
+import  PackagingDesign from '../Services_icons/MultiMedia/Packaging Design.webp'
+import  PhotoEditing from '../Services_icons/MultiMedia/Photo Editing.webp'
+import  PosterandFlyerDesign from '../Services_icons/MultiMedia/Poster and Flyer Design.webp'
+import  PrintDesign from '../Services_icons/MultiMedia/Print Design.webp'
+import  RenderingandVisualizationBannerDesign from '../Services_icons/MultiMedia/Rendering and Visualization.webp'
+import  SignageandBillboardDesign from '../Services_icons/MultiMedia/Signage and Billboard Design.webp'
+import  SocialMediaGraphics from '../Services_icons/MultiMedia/Social Media Graphics.webp'
+import  TShirtandMerchandiseDesign from '../Services_icons/MultiMedia/TShirt and Merchandise Design.webp'
+import  UserExperienceDesign from '../Services_icons/MultiMedia/User Experience Design.webp'
+import  UserInterfaceDesign from '../Services_icons/MultiMedia/User Interface Design.webp'
+import  WebDesign from '../Services_icons/MultiMedia/Web Design.webp'
+
+
+import DisplayAdvertising from '../Services_icons/Advertising/Display Advertising.webp'
+import InGameAdv from '../Services_icons/Advertising/In-Game Advertising.webp'
+import NativeAdv from '../Services_icons/Advertising/Native Advertising.webp'
+import OutDoorAdv from '../Services_icons/Advertising/Outdoor Advertising.webp'
+import PrintAdv from '../Services_icons/Advertising/Print Advertising.webp'
+import PrograAdv from '../Services_icons/Advertising/Programmatic Advertising.webp'
+import RadioAdv from '../Services_icons/Advertising/Radio Advertising.webp'
+import TheaterAdv from '../Services_icons/Advertising/Theatre Advertising.webp'
+import TvAdv from '../Services_icons/Advertising/TV Advertising.webp'
+import VideoAdv from '../Services_icons/Advertising/Video Advertising.webp'
+
+import BrandAwarness from '../Services_icons/Branding/Brand Awareness Campaigns.webp'
+import BrandExp from '../Services_icons/Branding/Brand Experience Design.webp'
+import BrandGuide from '../Services_icons/Branding/Brand Guidelines.webp'
+import BrandID from '../Services_icons/Branding/Brand Identity Exclusive.webp'
+import BrandLogo from '../Services_icons/Branding/Brand Logo Design.webp'
+import BrandMessage from '../Services_icons/Branding/Brand Messaging.webp'
+import BrandName from '../Services_icons/Branding/Brand Naming.webp'
+import BrandPosition from '../Services_icons/Branding/Brand Positioning.webp'
+import BrandResearch from '../Services_icons/Branding/Brand Research and Analysis.webp'
+import BrandStory from '../Services_icons/Branding/Brand Storytelling.webp'
+import BrandTracking from '../Services_icons/Branding/Brand Tracking and perfomance Evaluation.webp'
+import Rebranding from '../Services_icons/Branding/Rebranding Services.webp'
+import VisualID from '../Services_icons/Branding/Visual Identity Design.webp'
+
+import AndroidAppDevelopment from '../Services_icons/MobileApp/AndroidAppDevelopment.webp'
+import AppMaintenace from '../Services_icons/MobileApp/App Maintenance & Support.webp'
+import AppMonetization from '../Services_icons/MobileApp/App Monetization Strategy.webp'
+import AppPrototype from '../Services_icons/MobileApp/App Prototyping.webp'
+import AppSecurity from '../Services_icons/MobileApp/App Security.webp'
+import AppStore from '../Services_icons/MobileApp/App Store Optimization.webp'
+import AppTesting from '../Services_icons/MobileApp/App Testing & Quality.webp'
+import CrossPlatform from '../Services_icons/MobileApp/CrossPlatformAppDevelopment.webp'
+import EcommerceApp from '../Services_icons/MobileApp/Ecommerce App Dev.webp'
+import IosApp from '../Services_icons/MobileApp/iOS-App Development.webp'
+import MobileApp from '../Services_icons/MobileApp/Mobile App.webp'
+
+
 const GlobalData = {
   company: {
     companyName: 'Cyber Space Digital',
@@ -172,122 +232,122 @@ const GlobalData = {
 
   MultiMediaServices: [
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: logodesign, // Replace with the actual image source
       title: 'Logo Design',
       slug: '/logo-design',
       buttonText: 'Get Quotation',
     },
     
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandIdentityDesign, // Replace with the actual image source
       title: 'Brand Identity Design',
       slug: '/brand-identity-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: PrintDesign, // Replace with the actual image source
       title: 'Print Design',
       slug: '/print-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: WebDesign, // Replace with the actual image source
       title: 'Web Design',
       slug: '/web-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: UserInterfaceDesign, // Replace with the actual image source
       title: 'User Interface Design',
       slug: '/user-interface-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: UserExperienceDesign, // Replace with the actual image source
       title: 'User Experience Design',
       slug: '/user-experience-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: PackagingDesign, // Replace with the actual image source
       title: 'Packaging Design',
       slug: '/packaging-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: PosterandFlyerDesign, // Replace with the actual image source
       title: 'Poster and Flyer Design',
       slug: '/poster-and-flyer-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrochureDesign, // Replace with the actual image source
       title: 'Brochure Design',
       slug: '/brochure-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BannerDesign, // Replace with the actual image source
       title: 'Banner Design',
       slug: '/banner-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: IllustationServices, // Replace with the actual image source
       title: 'Illustation Services',
       slug: '/illustration-services',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: InfographicDesign, // Replace with the actual image source
       title: 'Infographic Design',
       slug: '/infographic-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: SocialMediaGraphics, // Replace with the actual image source
       title: 'Social Media Graphics',
       slug: '/social-media-graphics',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: eBookandPrintBook, // Replace with the actual image source
       title: 'eBook and Print Book Cover Design',
       slug: '/ebook-and-print-book-cover-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: TShirtandMerchandiseDesign, // Replace with the actual image source
       title: 'TShirt and Merchandise Design',
       slug: '/tshirt-and-merchandise-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
-      title: 'Signage and Billboard Design',
+      image: SignageandBillboardDesign, // Replace with the actual image source
+      title: 'Signage and Bill board Design',
       slug: '/signage-and-billboard-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BusinessCard, // Replace with the actual image source
       title: 'Business Card and Stationery Design',
       slug: '/business-card-and-stationery-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: RenderingandVisualizationBannerDesign, // Replace with the actual image source
       title: 'Rendering and Visualization',
       slug: '/rendering-and-visualization',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: MotionGraphics, // Replace with the actual image source
       title: 'Motion Graphics and Animation',
       slug: '/motion-graphics-and-animation',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: PhotoEditing, // Replace with the actual image source
       title: 'Photo Editing and Retouching',
       slug: '/phot-editing-and-retouching',
       buttonText: 'Get Quotation',
@@ -296,80 +356,80 @@ const GlobalData = {
 
   BrandingServices: [
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandID, // Replace with the actual image source
       title: 'Brand Identity Design Exclusive',
       slug: '/brand-identity-design-exclusive',
       buttonText: 'Get Quotation',
     },
     
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandLogo, // Replace with the actual image source
       title: 'Brand Logo Design',
       slug: '/brand-logo-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandGuide, // Replace with the actual image source
       title: 'Brand Guidelines',
       slug: '/brand-guidelines',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandPosition, // Replace with the actual image source
       title: 'Brand Positioning',
       slug: '/brand-positioning',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandStory, // Replace with the actual image source
       title: 'Brand Storytelling',
       slug: '/brand-storytelling',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandName, // Replace with the actual image source
       title: 'Brand Naming',
       slug: '/brand-naming',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: VisualID, // Replace with the actual image source
       title: 'Visual Identity Design',
       slug: '/visual-identity-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandMessage, // Replace with the actual image source
       title: 'Brand Messaging',
       slug: '/brand-messaging',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandExp, // Replace with the actual image source
       title: 'Brand Experience Design',
       slug: '/brand-experience-design',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandAwarness, // Replace with the actual image source
       title: 'Brand Awareness Campaigns',
       slug: '/brand-awareness-campaigns',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: Rebranding, // Replace with the actual image source
       title: 'Rebranding Services',
       slug: '/rebranding-services',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandResearch, // Replace with the actual image source
       title: 'Brand Research and Analysis',
       slug: '/brand-research-and-analysis',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: BrandTracking, // Replace with the actual image source
       title: 'Brand Tracking and perfomance Evaluation',
       slug: '/brand-tracking-and-perfomance-evaluation',
       buttonText: 'Get Quotation',
@@ -378,62 +438,62 @@ const GlobalData = {
 
   AdvertisingServices: [
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: DisplayAdvertising, // Replace with the actual image source
       title: 'Display Advertising',
       slug: '/display-advertising',
       buttonText: 'Get Quotation',
     },
     
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: VideoAdv, // Replace with the actual image source
       title: 'Video Advertising',
       slug: '/video-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: NativeAdv, // Replace with the actual image source
       title: 'Native Advertising',
       slug: '/native-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: PrograAdv, // Replace with the actual image source
       title: 'Programmatic Advertising',
       slug: '/programmatic-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: OutDoorAdv, // Replace with the actual image source
       title: 'Outdoor Advertising',
       slug: '/outdoor-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: PrintAdv, // Replace with the actual image source
       title: 'Print Advertising',
       slug: '/print-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: RadioAdv, // Replace with the actual image source
       title: 'Radio Advertising',
       slug: '/Radio-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: TvAdv, // Replace with the actual image source
       title: 'TV Advertising',
       slug: '/tv-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: InGameAdv, // Replace with the actual image source
       title: 'In-Game Advertising',
       slug: '/in-game-advertising',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, // Replace with the actual image source
+      image: TheaterAdv, // Replace with the actual image source
       title: 'Theatre Advertising',
       slug: '/theatre-advertising',
       buttonText: 'Get Quotation',
@@ -442,7 +502,7 @@ const GlobalData = {
 
   MobileappServices: [
     {
-      image: SearchEngineOptimization, 
+      image: MobileApp, 
       title: 'Mobile App',
       subtitle: 'Optimize your website for search engines.',
       slug: '/mobile-app-design',
@@ -450,70 +510,70 @@ const GlobalData = {
     },
     
     {
-      image: SearchEngineOptimization, 
+      image: IosApp, 
       title: 'iOS-App Development',
       subtitle: 'Create and distribute valuable content.',
       slug: '/ios-application-development',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
-      title: 'AndroidAppDevelopment',
+      image: AndroidAppDevelopment, 
+      title: 'Android App Development',
       subtitle: 'Promote your brand on social platforms.',
       slug: '/android-app-development',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: CrossPlatform, 
       title: 'CrossPlatformAppDevelopment',
       subtitle: 'Engage your audience with email campaigns.',
       slug: '/cross-platform-app-development',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: AppPrototype, 
       title: 'App Prototyping',
       subtitle: 'Drive traffic with targeted ads.',
       slug: '/app-prototyping',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: AppTesting, 
       title: 'App Testing & Quality Assurance',
       subtitle: 'Boost sales through affiliate programs.',
       slug: '/app-testing-and-quality-assurance',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: AppMaintenace, 
       title: 'App Maintenance & Support',
       subtitle: 'Gain insights from data for better strategies.',
       slug: '/app-maintenance-and-support',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: AppSecurity, 
       title: 'App Security',
       subtitle: 'Improve website conversion rates.',
       slug: '/app-security',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: AppMonetization, 
       title: 'App Monetization Strategy',
       subtitle: 'Target users on mobile devices.',
       slug: '/app-monetization-strategy',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: AppStore, 
       title: 'App Store Optimization',
       subtitle: 'Use videos for effective marketing.',
       slug: '/app-store-optimization',
       buttonText: 'Get Quotation',
     },
     {
-      image: SearchEngineOptimization, 
+      image: EcommerceApp, 
       title: 'Ecommerce App Development',
       subtitle: 'Enhance local online visibility.',
       slug: '/ecommerce-app-development',
