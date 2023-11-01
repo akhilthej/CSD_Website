fda904c95156ca326c51ba142cd8af90c92eaf4e
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-11-02 00:44:05 +0530

    onClick={() => setIsMenuOpen(false)}

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,6 +35,7 @@ function classNames(...classes) {
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const NavImages = {
     
@@ -1059,20 +1060,21 @@ const mobileappLinks = [
 <li className="p-4 lg:hidden">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="text-3xl" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? 'x' : '☰'}
+          <Menu.Button className="text-3xl" onClick={() => setIsMenuOpen(!isMenuOpen)} >
+            {isMenuOpen ? 'x' : '☰'}
           </Menu.Button>
         </div>
 
     <Transition
       as={Fragment}
-      enter="transition ease-out duration-100"
-      enterFrom="transform opacity-0 scale-95"
-      enterTo="transform opacity-100 scale-100"
-      leave="transition ease-in duration-75"
-      leaveFrom="transform opacity-100 scale-100"
-      leaveTo="transform opacity-0 scale-95"
-    >
+          show={isMenuOpen}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
       <Menu.Items className="text-center origin-top-right fixed sm:absolute right-0 mt-4 w-screen sm:w-56 h-screen sm:h-auto sm:rounded-md shadow-lg bg-gray-300 ring-black ring-opacity-5 divide-y divide-gray-400 z-50">
         <div>
         <Menu as="div" className="relative inline-block text-left">
@@ -1095,24 +1097,27 @@ const mobileappLinks = [
        <div className="flex flex-wrap">
      {/* Left Column */}
     <div className="w-1/2">
-      <Menu.Item>
-        {({ active }) => (
-          <Link to="/best-digital-marketing-company">
-            <a
-              className={classNames(
-                active ? " text-white" : "text-white font-bold",
-                "block py-10 text-sm " 
-              )} style={{
-          backgroundImage:
-            'url("https://drive.google.com/uc?id=1rwFsa4DIUEC04TDMp_8iQMxgSTGyEn9m")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}>
-              Digital Marketing
-            </a>
-          </Link>
-        )}
-      </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link to="/best-digital-marketing-company">
+                  <a
+                    className={classNames(
+                      active ? "text-white" : "text-white font-bold",
+                      "block py-10 text-sm"
+                    )}
+                    style={{
+                      backgroundImage:
+                        'url("https://drive.google.com/uc?id=1rwFsa4DIUEC04TDMp_8iQMxgSTGyEn9m")',
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                    }}
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Digital Marketing
+                  </a>
+                </Link>
+              )}
+            </Menu.Item>
       <Menu.Item>
         {({ active }) => (
           <Link to="/best-web-developement-company">
@@ -1125,7 +1130,7 @@ const mobileappLinks = [
             'url("https://drive.google.com/uc?id=1YOJ-Sjp3oiidK-JivRLm10UzunYuryOg")',
           backgroundSize: "cover",
           backgroundPosition: "center",
-        }}>
+        }}  onClick={() => setIsMenuOpen(false)}>
               Web Development
             </a>
           </Link>
@@ -1143,7 +1148,7 @@ const mobileappLinks = [
             'url("https://drive.google.com/uc?id=14rC7T12fy5Kr7LKS0_f1PNGazeqGdu8Y")',
           backgroundSize: "cover",
           backgroundPosition: "center",
-        }}>
+        }}  onClick={() => setIsMenuOpen(false)}>
               MultiMedia
             </a>
           </Link>
@@ -1165,7 +1170,7 @@ const mobileappLinks = [
             'url("https://drive.google.com/uc?id=1QK8qr84IZRLXiFvrWz7kVrqHpjq_7Ms9")',
           backgroundSize: "cover",
           backgroundPosition: "center",
-        }}>
+        }}  onClick={() => setIsMenuOpen(false)}>
               Branding
             </a>
           </Link>
@@ -1183,7 +1188,7 @@ const mobileappLinks = [
             'url("https://drive.google.com/uc?id=1mboisCG0oJ4En5-iOFqt3ma0WLTb7Y6d")',
           backgroundSize: "cover",
           backgroundPosition: "center",
-        }}>
+        }}  onClick={() => setIsMenuOpen(false)}>
               Advertisements
             </a>
           </Link>
@@ -1201,7 +1206,7 @@ const mobileappLinks = [
             'url("https://drive.google.com/uc?id=1E3eQlScwaZ2DGrZPqN5Ve7qaixUaCKRc")',
           backgroundSize: "cover",
           backgroundPosition: "center",
-        }}>
+        }}  onClick={() => setIsMenuOpen(false)}>
               Mobile App
             </a>
           </Link>
@@ -1216,7 +1221,7 @@ const mobileappLinks = [
               className={classNames(
                 active ? "  text-blue-700 bg-gray-500 " : "text-blue-700 bg-gray-300",
                 "block py-2 text-xs " 
-              )} >
+              )}  onClick={() => setIsMenuOpen(false)}>
               VIEW ALL SERVICES ➤
             </a>
           </Link>
