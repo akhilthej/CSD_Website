22535d5f227fd25fb5ee23ffd41cf758c1ddced6
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-11-02 21:34:52 +0530

    inner sections

--- src/assets/data/GlodalData.jsx
+++ src/assets/data/GlodalData.jsx
@@ -83,9 +83,10 @@ import MobileApp from '../Services_icons/MobileApp/Mobile App.webp'
 const GlobalData = {
   company: {
     companyName: 'Cyber Space Digital',
-    companyPhone: '+91-81434-07758',
+    companyPhone: '+918143407758',
     companyEmail: 'admin@cyberspacedigital.in',
     companyAddress: 'Visakhapatnam, Andhra Pradesh 530017',
+    companyWhatsapp: 'https://api.whatsapp.com/send?phone=918143407758',
   },
 
   WebDevelopmentServices: [
