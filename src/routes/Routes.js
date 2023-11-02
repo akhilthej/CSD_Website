185dcce31e5f769431f72ac56dc879c92bb850ef
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-11-02 21:35:45 +0530

    1

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,11 @@
 import Navbar from "../components/Navbar/Navbar";
 import Footer from '../components/Footer/Footer';
 
-import PrivacyPolicy from '../components/PAGES/Documents/PrivacyPolicy';
-import TermsnConditions from '../components/PAGES/Documents/TermsnConditions';
-import ReturnPolicy from '../components/PAGES/Documents/ReturnsPolicy';
-import ShippingandDelivery from '../components/PAGES/Documents/ShippingandDelivery';
-import Disclaimer from '../components/PAGES/Documents/Disclaimer';
+import PrivacyPolicy from '../components/Documents/PrivacyPolicy';
+import TermsnConditions from '../components/Documents/TermsnConditions';
+import ReturnPolicy from '../components/Documents/ReturnsPolicy';
+import ShippingandDelivery from '../components/Documents/ShippingandDelivery';
+import Disclaimer from '../components/Documents/Disclaimer';
 
 import Error404 from '../components/Tools/Error404';
 import Home from './HOME';
