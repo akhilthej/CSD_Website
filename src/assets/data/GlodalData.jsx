b80462b85fa368e6e4a31fc86afdaff8a6a7661c
akhilthej <67720804+akhilthej@users.noreply.github.com>
2025-05-05 08:37:34 +0530

    1

--- src/assets/data/GlodalData.jsx
+++ src/assets/data/GlodalData.jsx
@@ -85,7 +85,7 @@ const GlobalData = {
   company: {
     companyName: 'Cyber Space Digital',
     companyPhone: '+918143407758',
-    companyEmail: 'admin@cyberspacedigital.in',
+    companyEmail: 'mail@cyberspacedigital.in',
     companyAddress: 'Visakhapatnam, Andhra Pradesh 530017',
     companyWhatsapp: 'https://api.whatsapp.com/send?phone=918143407758',
   },
